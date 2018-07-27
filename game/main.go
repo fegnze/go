cb97d681de4ee9df54e133e013d394c331ec8e63
fegnze <fegnze@outlook.com>
2018-07-27 18:06:10 +0800

    1

---
+++ game/main.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"time"
+
+	"./net/http"
+)
+
+func main() {
+	start := time.Now()
+	ch := make(chan string)
+	for _, url := range os.Args[1:] {
+		go http.Fetch(url, ch)
+	}
+
+	for range os.Args[1:] {
+		fmt.Println(<-ch)
+	}
+
+	fmt.Printf("%.2fs elapsed\n", time.Since(start).Seconds())
+}
